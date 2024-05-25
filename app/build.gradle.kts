508c355b4f6d75e1b022ee6127a1bef21d68efa7
Eduardo Soto <mesg1911@outlook.es>
2024-05-25 14:14:00 -0600

    Funcionalidad Añadida a Ubicaicones
    Funcionan los Vínculos de la lista con las ubicaciones, falta estandarizar los eventos y donde se llevan a cabo

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,16 +3,18 @@ plugins {
     id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin")
 }
 
+
 android {
     namespace = "com.example.itour_release"
     compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.itour_release"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
+        useLibrary("org.apache.http.legacy")
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -33,6 +35,7 @@ android {
     buildFeatures {
         viewBinding = true
     }
+
 }
 
 dependencies {
@@ -49,6 +52,8 @@ dependencies {
     implementation("com.google.android.gms:play-services-maps:18.2.0")
     implementation("com.google.firebase:firebase-inappmessaging:20.4.0")
     implementation("com.google.android.libraries.places:places:3.3.0")
+    implementation("androidx.activity:activity:1.8.0")
+    implementation("com.google.firebase:firebase-crashlytics-buildtools:3.0.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
